f7639890bf640aeb0b55c8fe62e6f0f83e3fda76
Wellington Mereles <camposwellintom2@gmail.com>
2021-08-17 23:32:46 -0300

    First commit

---
+++ Program.cs
@@ -0,0 +1,120 @@
+﻿using System;
+
+namespace Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Menu();
+        }
+
+        static void Menu()
+        {
+            Console.Clear();
+
+            Console.WriteLine("-----------------------");
+            Console.WriteLine("Qual opção você deseja?");
+            Console.WriteLine("-----------------------");
+            Console.WriteLine("1 - Soma");
+            Console.WriteLine("2 - Subtração");
+            Console.WriteLine("3 - Divisão");
+            Console.WriteLine("4 - Multiplicação");
+            Console.WriteLine("5 - Sair");
+
+            Console.WriteLine("-----------------------");
+            Console.WriteLine("Escolha uma opção: ");
+            short res = short.Parse(Console.ReadLine());
+
+            switch (res)
+            {
+                case 1: Soma(); break;
+                case 2: Subtracao(); break;
+                case 3: Divisao(); break;
+                case 4: Multiplicacao(); break;
+                case 5: System.Environment.Exit(0); break;
+                default: Menu(); break;
+            }
+
+        }
+
+        static void Soma()
+        {
+            Console.Clear();
+
+            Console.WriteLine("Você escolheu a opção soma");
+            Console.WriteLine("Digite o primeiro valor: ");
+            double v1 = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite o segundo valor: ");
+            double v2 = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("------------------------");
+
+            double resultado = v1 + v2;
+            // Console.WriteLine("O resultado da soma é " + resultado);
+            Console.WriteLine($"O resultado da soma é {resultado}");
+            // Console.WriteLine($"O resultado da soma é {v1 + v2}");
+            // Console.WriteLine("O resultado da soma é " + (v1 + v2));
+            Console.ReadKey();
+            Menu();
+        }
+
+        static void Subtracao()
+        {
+            Console.Clear();
+
+            Console.WriteLine("Você escolheu a opção subtração");
+            Console.WriteLine("Digite o primeiro valor: ");
+            double v1 = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite o segundo valor: ");
+            double v2 = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("------------------------");
+
+            double resultado = v1 - v2;
+            Console.WriteLine($"O resultado da subtração é {resultado}");
+            Console.ReadKey();
+            Menu();
+        }
+
+        static void Divisao()
+        {
+            Console.Clear();
+
+            Console.WriteLine("Você escolheu a opção divisão");
+            Console.WriteLine("Digite o primeiro valor: ");
+            double v1 = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite o segundo valor: ");
+            double v2 = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("------------------------");
+
+            double resultado = v1 / v2;
+            Console.WriteLine($"O resultado da divisão é {resultado}");
+            Console.ReadKey();
+            Menu();
+        }
+
+        static void Multiplicacao()
+        {
+            Console.Clear();
+
+            Console.WriteLine("Você escolheu a opção multiplicação");
+            Console.WriteLine("Digite o primeiro valor: ");
+            double v1 = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite o segundo valor: ");
+            double v2 = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("------------------------");
+
+            double resultado = v1 * v2;
+            Console.WriteLine("O resultado da multiplicação é " + (v1 * v2));
+            Console.ReadKey();
+            Menu();
+        }
+    }
+}
