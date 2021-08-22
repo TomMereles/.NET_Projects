e6c029d2fc7089f83e3ec59d9416d3d4547c2b9c
Wellington Mereles <camposwellintom2@gmail.com>
2021-08-22 00:39:51 -0300

    Add. timezone

---
+++ Timezone/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Timezone
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Clear();
+
+            var utcDate = DateTime.UtcNow;
+
+            var timezones = TimeZoneInfo.GetSystemTimeZones();
+            foreach (var timezone in timezones)
+            {
+                Console.WriteLine(timezone.Id);
+                Console.WriteLine(timezone);
+                Console.WriteLine(TimeZoneInfo.ConvertTimeFromUtc(utcDate, timezone));
+                Console.WriteLine("__________");
+            
+            } 
+
+        }
+    }
+}
